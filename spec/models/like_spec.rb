effda9607e27a97f314c59d259c46daea6c95cbf
husush23 <husseinkadare40@gmail.com>
2023-08-24 17:35:17 +0300

    Added required changes

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
+  let(:user) { User.create(name: 'Hussein Ali') }
+  let(:post) { Post.create(author: user, title: 'My Post', text: 'This is my post content.') }
+
+  it 'increments the likes_counter of the associated post after saving a like' do
+    like = Like.create(author: user, post: post)
+    original_likes_counter = post.likes_counter
+    like.save
+    post.reload
+    expect(post.likes_counter).to eq(original_likes_counter + 1)
+  end
+  
   it 'is valid with valid attributes' do
     user = User.create(name: 'Hussein Ali')
     post = Post.create(author: user, title: 'My Post', text: 'This is my post content.')
