2de423ee1232048b2fa6608c03cbef417466250a
husush23 <husseinkadare40@gmail.com>
2023-08-24 09:15:48 +0300

    Updated

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,9 +13,9 @@ class Post < ApplicationRecord
   def recent_comments(limit = 5)
     comments.order(created_at: :desc).limit(limit)
   end
-  
+
   private
-  
+
   def increment_author_posts_counter
     puts 'Callback invoked!'
     author.increment!(:posts_counter)
