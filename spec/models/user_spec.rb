a7969dd096e29018ec1ad97b5deed6d49a2da3c1
husush23 <husseinkadare40@gmail.com>
2023-08-24 17:43:15 +0300

    Fixed errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,33 +11,32 @@ RSpec.describe User, type: :model do
     expect(user).not_to be_valid
   end
 
-  it "is valid with a zero posts_counter" do
-    user = User.new(name: "John Doe", posts_counter: 0)
-    expect(user).to be_valid
-  end
-  
-  it "is valid with a positive posts_counter" do
-    user = User.new(name: "John Doe", posts_counter: 10)
+  it 'is valid with a zero posts_counter' do
+    user = User.new(name: 'John Doe', posts_counter: 0)
     expect(user).to be_valid
   end
 
-  it "is not valid with a negative posts_counter" do
-    user = User.new(name: "John Doe", posts_counter: -1)
+  it 'is valid with a positive posts_counter' do
+    user = User.new(name: 'John Doe', posts_counter: 10)
+    expect(user).to be_valid
+  end
+
+  it 'is not valid with a negative posts_counter' do
+    user = User.new(name: 'John Doe', posts_counter: -1)
     expect(user).not_to be_valid
-    expect(user.errors[:posts_counter]).to include("must be greater than or equal to 0")
+    expect(user.errors[:posts_counter]).to include('must be greater than or equal to 0')
   end
 
-  it "returns the 3 most recent posts" do
-    user = User.create(name: "John Doe")
-    post1 = Post.create(author: user, title: "Post 1", text: "Text 1")
-    post2 = Post.create(author: user, title: "Post 2", text: "Text 2")
-    post3 = Post.create(author: user, title: "Post 3", text: "Text 3")
-    post4 = Post.create(author: user, title: "Post 4", text: "Text 4")
+  it 'returns the 3 most recent posts' do
+    user = User.create(name: 'John Doe')
+    post1 = Post.create(author: user, title: 'Post 1', text: 'Text 1')
+    post2 = Post.create(author: user, title: 'Post 2', text: 'Text 2')
+    post3 = Post.create(author: user, title: 'Post 3', text: 'Text 3')
+    post4 = Post.create(author: user, title: 'Post 4', text: 'Text 4')
 
     recent_posts = user.recent_posts
 
     expect(recent_posts).to eq([post4, post3, post2])
+    expect(recent_posts).not_to include(post1)
   end
-
-  
 end
