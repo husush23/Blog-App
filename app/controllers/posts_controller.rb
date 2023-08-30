ce87d4e21062454eab2d95ca85f1c88cd404d41b
husush23 <husseinkadare40@gmail.com>
2023-08-30 10:51:50 +0300

    Basic structure created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,11 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:id])
+    @posts = @user.posts
+  end
 
   def show
-    render 'show'
+    @post = Post.find(params[:id]) # Find the post by the post ID
+    @user = @post.author            # Fetch the user who authored the post
   end
 end
