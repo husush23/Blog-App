7145cb795a60aaddb8c52b01f4b83fc57ed701ec
husush23 <husseinkadare40@gmail.com>
2023-08-31 16:31:48 +0300

    Updated the routes to use rails conventions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,18 @@
 class PostsController < ApplicationController
+  before_action :find_user
+
   def index
-    @user = User.find(params[:id])
     @posts = @user.posts.includes(comments: :author)
   end
 
   def show
     @post = Post.includes(comments: :author).find(params[:id])
     @user = @post.author
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:user_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
+# Rails.application.routes.draw do
+#   root to: 'users#index'
+#   get 'users', to: 'users#index'
+#   get 'users/:id', to: 'users#show'
+#   get 'users/:id/posts', to: 'posts#index'
+#   get 'users/:id/posts/:id', to: 'posts#show'
+# end
 Rails.application.routes.draw do
   root to: 'users#index'
-  get 'users', to: 'users#index'
-  get 'users/:id', to: 'users#show'
-  get 'users/:id/posts', to: 'posts#index'
-  get 'users/:id/posts/:id', to: 'posts#show'
-  get '/new', to: 'users#new'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
+
