e8540030c84487b40edab889f5d4c8be57ab75c2
husush23 <husseinkadare40@gmail.com>
2023-08-31 13:48:57 +0300

    Form created and user controller updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,23 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create 
+    @user = User.new(user_params)
+
+    if @user.save 
+      redirect_to @user, notic: 'User was successfully created'
+    else
+      render :new
+    end
+  end
+
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
+  end
+
+  private 
+  def user_params
+    params.require(:user).permit(name:, photo:, bio:)
   end
 end
