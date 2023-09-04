84e1f11e7522d661d84d7cce0862da80505d780e
husush23 <husseinkadare40@gmail.com>
2023-09-04 23:27:10 +0300

    users tests added

--- spec/features/users_index_spec.rb
+++ spec/features/users_index_spec.rb
@@ -2,30 +2,42 @@ require 'rails_helper'
 
 RSpec.describe 'Users Index Page', type: :feature do
   before do
-    # Create some sample users for testing
     @user1 = User.create(name: 'User 1', photo: 'https://example.com/photo1.jpg', posts_counter: 3)
     @user2 = User.create(name: 'User 2', photo: 'https://example.com/photo2.jpg', posts_counter: 5)
-
-    visit users_path
   end
 
-  it 'displays a list of users' do
-    expect(page).to have_content('Users list page')
-    expect(page).to have_content('User 1')
-    expect(page).to have_content('User 2')
+  describe 'User contents' do
+    before(:example) { visit users_path }
+    
+    it 'displays a list of users' do
+      expect(page).to have_content('Users list page')
+      expect(page).to have_content('User 1')
+      expect(page).to have_content('User 2')
+    end
 
-  end
+    it 'displays user details' do
+      expect(page).to have_content('User 1')
+      expect(page).to have_content('User 2')
+    end
 
-  it 'displays user details' do
-    expect(page).to have_content('User 1')
-    expect(page).to have_content('User 2')
-  end
-  it 'displays photos' do
-  expect(page).to have_css("img[src*='photo1.jpg']")
-  end
+    it 'displays photos' do
+      expect(page).to have_css("img[src*='photo1.jpg']")
+      expect(page).to have_css("img[src*='photo2.jpg']")
+    end
 
-  it 'Displays how many post each user own' do
-    expect(page).to have_content('Number of posts: 3')
-    expect(page).to have_content('Number of posts: 5')
+    it 'displays how many posts each user owns' do
+      expect(page).to have_content('Number of posts: 3')
+      expect(page).to have_content('Number of posts: 5')
+    end
+
+    it 'displays user names as clickable links' do
+      expect(page).to have_link(@user1.name, href: user_path(@user1))
+      expect(page).to have_link(@user2.name, href: user_path(@user2))
+    end
+
+    it 'redirects to the user profile page when clicking on a user name' do
+      click_link(@user1.name)
+      expect(page).to have_current_path(user_path(@user1))
+    end
   end
 end
