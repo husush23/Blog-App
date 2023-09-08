e8a08ce75554878dc25e76ba705cea58b2e55089
Peter Dumbari <dummytwiz@gmail.com>
2023-09-08 08:16:12 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,11 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :users, only: [:index, :show] do
+        resources :posts, only: [:index, :show] do
+          resources :comments, only: [:index, :create]
+        end
+      end
     end
   end
   
