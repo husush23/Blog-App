0190cb81be3a778ecb1465d303d2aa4720387c37
Hussein Hashi <husseinkadare40@gmail.com>
2023-09-08 10:02:30 +0300

    Update comments_controller.rb

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,1 +1,30 @@
+class Api::V1::CommentsController < ApplicationController
+    before_action :set_user
+    before_action :set_post
+  
+    def index
+      @comments = @post.comments
+      render json: @comments
+    end
+  
+    def show
+      @comment = @post.comments.find(params[:id])
+      render json: @comment
+    end
+
+  
+    private
+  
+    def set_user
+      @user = User.find(params[:user_id])
+    end
+  
+    def set_post
+      @post = @user.posts.find(params[:post_id])
+    end
+  
+    def comment_params
+      params.require(:comment).permit(:text)
+    end
+  end
 
